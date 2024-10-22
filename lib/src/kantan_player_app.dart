e7674ace0dee68c4c9606082c3cc02bf6b898b2b
Christopher Perry <ozbonus@gmail.com>
2024-10-22 18:05:03 +0800

    Robust startup.

--- lib/src/kantan_player_app.dart
+++ lib/src/kantan_player_app.dart
@@ -1,14 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kantan/src/features/settings/data/settings_repository.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'kantan_player_app.g.dart';
 
 class KantanPlayerApp extends StatelessWidget {
   const KantanPlayerApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: FlutterLogo(),
+    return const _EagerInitializer(
+      child: MaterialApp(
+        home: Scaffold(
+          body: TestHome(),
+        ),
       ),
     );
+  }
+}
+
+class TestHome extends ConsumerWidget {
+  const TestHome({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}
+
+@riverpod
+FutureOr<void> appStartup(AppStartupRef ref) async {
+  ref.onDispose(() {
+    ref.invalidate(settingsRepositoryProvider);
+  });
+  await ref.watch(settingsRepositoryProvider.future);
+}
+
+class _EagerInitializer extends ConsumerWidget {
+  const _EagerInitializer({required this.child});
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final appStartupState = ref.watch(appStartupProvider);
+    return appStartupState.when(
+      loading: () => const AppStartupLoading(),
+      error: (e, st) => const AppStartupError(),
+      data: (_) => child,
+    );
+  }
+}
+
+class AppStartupLoading extends StatelessWidget {
+  const AppStartupLoading({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CircularProgressIndicator.adaptive();
+  }
+}
+
+class AppStartupError extends StatelessWidget {
+  const AppStartupError({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
---
+++ lib/src/kantan_player_app.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'kantan_player_app.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$appStartupHash() => r'13bbac98599d1846e0918433ad81ed7d06a52c8c';
+
+/// See also [appStartup].
+@ProviderFor(appStartup)
+final appStartupProvider = AutoDisposeFutureProvider<void>.internal(
+  appStartup,
+  name: r'appStartupProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$appStartupHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef AppStartupRef = AutoDisposeFutureProviderRef<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
