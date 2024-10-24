192b0d918373c71476764f4d9e35d83b2b158eb6
Christopher Perry <ozbonus@gmail.com>
2024-10-24 11:54:52 +0800

    Add config for audio handler.

--- lib/config.dart
+++ lib/config.dart
@@ -1,3 +1,7 @@
 class Config {
-  static String get tracksJson => const String.fromEnvironment('TRACKS_JSON');
+  static String getConfig(String key) => String.fromEnvironment(key);
+
+  static String get tracksJson => getConfig('TRACKS_JSON');
+  static String get rewindDuration => getConfig('rewindDuration');
+  static String get fastForwardDuration => getConfig('fastForwardDuration');
 }
