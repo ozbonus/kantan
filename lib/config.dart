b4714819d9bfca4e4526a4817012c72a7e2f998b
Christopher Perry <ozbonus@gmail.com>
2024-10-25 11:11:09 +0800

    Add Android specific config parameters.

--- lib/config.dart
+++ lib/config.dart
@@ -2,6 +2,9 @@ class Config {
   static String getConfig(String key) => String.fromEnvironment(key);
 
   static String get tracksJson => getConfig('TRACKS_JSON');
+  static String get channelId => getConfig('androidNotificationChannelId');
+  static String get channelName => getConfig('androidNotificationChannelName');
+  static String get notificationIcon => getConfig('androidNotificationIcon');
   static String get rewindDuration => getConfig('rewindDuration');
   static String get fastForwardDuration => getConfig('fastForwardDuration');
 }
