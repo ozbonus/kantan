3832f6f7c6e65135dff9e5cc187b205bdedad34f
Christopher Perry <ozbonus@gmail.com>
2024-11-09 21:24:49 +0800

    Show a placeholder widget.

--- lib/src/kantan_player_app.dart
+++ lib/src/kantan_player_app.dart
@@ -7,7 +7,7 @@ class KantanPlayerApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Scaffold(),
+        body: Placeholder(),
       ),
     );
   }
