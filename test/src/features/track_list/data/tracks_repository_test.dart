b01e93989559590bca365837a8291ed6b3bc757d
Christopher Perry <ozbonus@gmail.com>
2024-11-12 15:42:10 +0800

    Test MediaItem list provider.

--- test/src/features/track_list/data/tracks_repository_test.dart
+++ test/src/features/track_list/data/tracks_repository_test.dart
@@ -1,3 +1,5 @@
+import 'package:audio_service/audio_service.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:kantan/src/features/track_list/data/tracks_repository.dart';
 import 'package:kantan/src/features/track_list/domain/track.dart';
@@ -42,6 +44,22 @@ final track2 = Track.fromMap(trackMap2);
 
 final trackList = [track1, track2];
 
+ProviderContainer createContainer({
+  ProviderContainer? parent,
+  List<Override> overrides = const [],
+  List<ProviderObserver>? observers,
+}) {
+  final container = ProviderContainer(
+    parent: parent,
+    overrides: overrides,
+    observers: observers,
+  );
+
+  addTearDown(container.dispose);
+
+  return container;
+}
+
 void main() {
   test('Create a normal list of tracks', () {
     const repository = TracksRepository(tracksMap);
@@ -54,5 +72,26 @@ void main() {
     final tracksList = repository.tracksList();
     expect(tracksList[0], equals(track1));
     expect(tracksList[1], equals(track2));
+  });
+
+  test('Make MediaItems', () {
+    final container = createContainer();
+    final mediaItemList = container.read(mediaItemListProvider(trackList));
+    print(mediaItemList[0]);
+    expect(mediaItemList, isA<List<MediaItem>>());
+  });
+
+  test('MediaItems are correct.', () {
+    final container = createContainer();
+    final mediaItemList = container.read(mediaItemListProvider(trackList));
+    final expectedMediaitem = MediaItem(
+      id: track1.filename,
+      album: track1.album,
+      title: track1.title,
+      displayDescription: track1.displayDescription,
+      artist: track1.artist,
+      duration: Duration(milliseconds: track1.duration),
+    );
+    expect(mediaItemList[0], equals(expectedMediaitem));
   });
 }
