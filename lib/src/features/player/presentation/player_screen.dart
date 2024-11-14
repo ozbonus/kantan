cbd17800de3440808f63847b522049583f47c55a
Christopher Perry <ozbonus@gmail.com>
2024-11-14 11:52:52 +0800

    Add repeat mode button to player screen.

--- lib/src/features/player/presentation/player_screen.dart
+++ lib/src/features/player/presentation/player_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:kantan/src/features/player/presentation/play_pause_button.dart';
 import 'package:kantan/src/features/player/presentation/prev_next_buttons.dart';
+import 'package:kantan/src/features/player/presentation/repeat_mode_button.dart';
 import 'package:kantan/src/features/player/presentation/rewind_forward_buttons.dart';
 import 'package:kantan/src/routing/app_router.dart';
 
@@ -35,6 +36,7 @@ class PlayerScreen extends ConsumerWidget {
               children: [
                 SkipToPreviousButton(),
                 SkipToNextButton(),
+                RepeatModeButton(),
               ],
             ),
             Padding(
