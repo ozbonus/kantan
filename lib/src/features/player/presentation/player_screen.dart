16733ec71ddbf96bfe412633020a9f27716fff21
Christopher Perry <ozbonus@gmail.com>
2024-11-15 10:18:15 +0800

    Add progress slider to player screen.

--- lib/src/features/player/presentation/player_screen.dart
+++ lib/src/features/player/presentation/player_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:kantan/src/features/player/presentation/play_pause_button.dart';
 import 'package:kantan/src/features/player/presentation/prev_next_buttons.dart';
+import 'package:kantan/src/features/player/presentation/progress_slider.dart';
 import 'package:kantan/src/features/player/presentation/repeat_mode_button.dart';
 import 'package:kantan/src/features/player/presentation/rewind_forward_buttons.dart';
 import 'package:kantan/src/routing/app_router.dart';
@@ -39,6 +40,7 @@ class PlayerScreen extends ConsumerWidget {
                 RepeatModeButton(),
               ],
             ),
+            const ProgressSlider(),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: FilledButton(
