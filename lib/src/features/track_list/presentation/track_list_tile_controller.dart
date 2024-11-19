7eb05ef67a1a33e370a9491a09dfd829528313be
Christopher Perry <ozbonus@gmail.com>
2024-11-19 17:35:42 +0800

    Have controller provide a bool.

---
+++ lib/src/features/track_list/presentation/track_list_tile_controller.dart
@@ -0,0 +1,18 @@
+import 'package:kantan/src/features/player/application/audio_handler_service.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'track_list_tile_controller.g.dart';
+
+@riverpod
+class TrackListTileController extends _$TrackListTileController {
+  @override
+  bool? build(int index) {
+    return ref.watch(queueIndexStreamProvider).whenOrNull(
+          data: (queueIndex) => queueIndex == index,
+        );
+  }
+
+  void skipToTrack(int index) {
+    ref.read(audioHandlerProvider).requireValue.skipToQueueItem(index);
+  }
+}
