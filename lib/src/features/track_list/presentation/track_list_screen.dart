ff1f2c65eed4f8de0277bed7a2dedb590db474a2
Christopher Perry <ozbonus@gmail.com>
2024-11-19 17:36:14 +0800

    Show track list.

--- lib/src/features/track_list/presentation/track_list_screen.dart
+++ lib/src/features/track_list/presentation/track_list_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:kantan/src/common_widgets/async_value_widget.dart';
 import 'package:kantan/src/features/player/application/audio_handler_service.dart';
+import 'package:kantan/src/features/track_list/data/tracks_repository.dart';
+import 'package:kantan/src/features/track_list/presentation/track_list_tile.dart';
 import 'package:kantan/src/routing/app_router.dart';
 
 class TrackListScreen extends ConsumerWidget {
@@ -14,50 +17,30 @@ class TrackListScreen extends ConsumerWidget {
       appBar: AppBar(
         title: const Text('Track List Screen'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                IconButton.filledTonal(
-                  onPressed: audioHandler.play,
-                  icon: const Icon(Icons.play_arrow_rounded),
-                ),
-                IconButton.filledTonal(
-                  onPressed: audioHandler.pause,
-                  icon: const Icon(Icons.pause_rounded),
-                ),
-                IconButton.filledTonal(
-                  onPressed: audioHandler.skipToPrevious,
-                  icon: const Icon(Icons.skip_previous_rounded),
-                ),
-                IconButton.filledTonal(
-                  onPressed: audioHandler.skipToNext,
-                  icon: const Icon(Icons.skip_next_rounded),
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: FilledButton(
-                onPressed: () => context.goNamed(AppRoute.player),
-                child: const Text('Go to Player Screen'),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: FilledButton(
-                onPressed: () => context.goNamed(AppRoute.transcript),
-                child: const Text('Go to Transcript Screen'),
-              ),
-            ),
-          ],
-        ),
-      ),
+      body: const TracksList(),
+    );
+  }
+}
+
+class TracksList extends ConsumerWidget {
+  const TracksList({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final tracksList = ref.watch(tracksListProvider);
+    return AsyncValueWidget(
+      value: tracksList,
+      data: (tracks) {
+        return ListView.builder(
+          itemCount: tracks.length,
+          itemBuilder: (context, index) {
+            return TrackListTile(
+              track: tracks[index],
+              index: index,
+            );
+          },
+        );
+      },
     );
   }
 }
