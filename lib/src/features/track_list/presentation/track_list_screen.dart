4fc037c1c43dfaa0daddf2cf4506857941267b83
Christopher Perry <ozbonus@gmail.com>
2024-11-21 09:38:37 +0800

    Remove unused code.

--- lib/src/features/track_list/presentation/track_list_screen.dart
+++ lib/src/features/track_list/presentation/track_list_screen.dart
@@ -1,20 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import 'package:kantan/config.dart';
 import 'package:kantan/src/common_widgets/async_value_widget.dart';
-import 'package:kantan/src/features/player/application/audio_handler_service.dart';
 import 'package:kantan/src/features/player/presentation/floating_mini_player.dart';
 import 'package:kantan/src/features/track_list/data/tracks_repository.dart';
 import 'package:kantan/src/features/track_list/presentation/track_list_tile.dart';
-import 'package:kantan/src/routing/app_router.dart';
 
 class TrackListScreen extends ConsumerWidget {
   const TrackListScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final audioHandler = ref.watch(audioHandlerProvider).requireValue;
     return Scaffold(
       appBar: AppBar(
         title: Text(Config.appTitle),
