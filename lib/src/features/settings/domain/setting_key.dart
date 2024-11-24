f45ac3656b21464559d20a8ffab18c5c0fb0f4cf
Christopher Perry <ozbonus@gmail.com>
2024-11-24 16:54:29 +0800

    Remove keys for debugging.

--- lib/src/features/settings/domain/setting_key.dart
+++ lib/src/features/settings/domain/setting_key.dart
@@ -1,8 +1,4 @@
 class SettingKey {
-  static const debugBool = 'debugBool';
-  static const debugInt = 'debugInt';
-  static const debugString = 'debugString';
-
   static const queueIndex = 'queueIndex';
   static const position = 'position';
   static const speed = 'speed';
