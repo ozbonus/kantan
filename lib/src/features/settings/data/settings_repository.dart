7ff696117dd0736ba64033e3eccb6dcf62415c1a
Christopher Perry <ozbonus@gmail.com>
2024-11-24 16:23:10 +0800

    Import flutter_riverpod, not riverpod.

--- lib/src/features/settings/data/settings_repository.dart
+++ lib/src/features/settings/data/settings_repository.dart
@@ -1,5 +1,5 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kantan/src/features/settings/domain/setting_key.dart';
-import 'package:riverpod/riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
