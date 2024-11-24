c72a163d97edc04a349073936159755b5bd55d90
Christopher Perry <ozbonus@gmail.com>
2024-11-24 16:50:52 +0800

    Remove debug methods.

--- lib/src/features/settings/data/settings_repository.dart
+++ lib/src/features/settings/data/settings_repository.dart
@@ -9,24 +9,9 @@ class SettingsRepository {
   const SettingsRepository(this.prefs);
   final SharedPreferences prefs;
 
-  bool? get debugBool => prefs.getBool(SettingKey.debugBool);
-  int? get debugInt => prefs.getInt(SettingKey.debugInt);
-  String? get debugString => prefs.getString(SettingKey.debugString);
   int? get track => prefs.getInt(SettingKey.track);
   int? get time => prefs.getInt(SettingKey.time);
   double? get speed => prefs.getDouble(SettingKey.speed);
-
-  Future<bool> setDebugBool(bool value) async {
-    return await prefs.setBool(SettingKey.debugBool, value);
-  }
-
-  Future<bool> setDebugInt(int value) async {
-    return await prefs.setInt(SettingKey.debugInt, value);
-  }
-
-  Future<bool> setDebugString(String value) async {
-    return await prefs.setString(SettingKey.debugString, value);
-  }
 
   Future<bool> setTrack(int value) async {
     return await prefs.setInt(SettingKey.track, value);
--- test/src/features/settings/data/settings_repository_test.dart
+++ test/src/features/settings/data/settings_repository_test.dart
@@ -5,9 +5,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 const Map<String, Object> nullValues = {};
 const Map<String, Object> fullValues = {
-  SettingKey.debugBool: true,
-  SettingKey.debugInt: 0,
-  SettingKey.debugString: 'fnord',
   SettingKey.track: 1,
   SettingKey.time: 1,
   SettingKey.speed: 1.0,
@@ -32,9 +29,6 @@ void main() {
 
   test('Get full values.', () async {
     final repository = await makeRepository(fullValues);
-    expect(repository.debugBool, true);
-    expect(repository.debugInt, 0);
-    expect(repository.debugString, 'fnord');
     expect(repository.track, 1);
     expect(repository.time, 1);
     expect(repository.speed, 1.0);
@@ -42,9 +36,6 @@ void main() {
 
   test('Get null values.', () async {
     final repository = await makeRepository(nullValues);
-    expect(repository.debugBool, null);
-    expect(repository.debugInt, null);
-    expect(repository.debugString, null);
     expect(repository.track, null);
     expect(repository.time, null);
     expect(repository.speed, null);
@@ -52,15 +43,9 @@ void main() {
 
   test('Write and read values.', () async {
     final repository = await makeRepository(nullValues);
-    expectLater(repository.setDebugBool(true), completion(true));
-    expectLater(repository.setDebugInt(1), completion(true));
-    expectLater(repository.setDebugString('fnord'), completion(true));
     expectLater(repository.setTrack(2), completion(true));
     expectLater(repository.setTime(3), completion(true));
     expectLater(repository.setSpeed(2.5), completion(true));
-    expect(repository.debugBool, true);
-    expect(repository.debugInt, 1);
-    expect(repository.debugString, 'fnord');
     expect(repository.track, 2);
     expect(repository.time, 3);
     expect(repository.speed, 2.5);
