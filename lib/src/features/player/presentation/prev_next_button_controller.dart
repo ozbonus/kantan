747b61f8c9f1793993c005f8b052c1a7e40cd922
Christopher Perry <ozbonus@gmail.com>
2025-04-12 16:12:36 +0800

    Simpler, separate providers.

--- lib/src/features/player/presentation/prev_next_button_controller.dart
+++ lib/src/features/player/presentation/prev_next_button_controller.dart
@@ -1,7 +1,18 @@
-import 'package:kantan/src/features/player/application/audio_handler_service.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:kantan/src/features/player/application/audio_handler_service.dart';
 
 part 'prev_next_button_controller.g.dart';
+
+@riverpod
+void skipToPreviousButtonController(Ref ref) {
+  ref.read(audioHandlerProvider).requireValue.skipToPrevious();
+}
+
+@riverpod
+void skipToNextButtonController(Ref ref) {
+  ref.read(audioHandlerProvider).requireValue.skipToNext();
+}
 
 @riverpod
 class PrevNextButtonController extends _$PrevNextButtonController {
--- lib/src/features/player/presentation/prev_next_button_controller.g.dart
+++ lib/src/features/player/presentation/prev_next_button_controller.g.dart
@@ -6,6 +6,43 @@ part of 'prev_next_button_controller.dart';
 // RiverpodGenerator
 // **************************************************************************
 
+String _$skipToPreviousButtonControllerHash() =>
+    r'ca64076447806952e2cee3abb14bc1877ffca1c0';
+
+/// See also [skipToPreviousButtonController].
+@ProviderFor(skipToPreviousButtonController)
+final skipToPreviousButtonControllerProvider =
+    AutoDisposeProvider<void>.internal(
+  skipToPreviousButtonController,
+  name: r'skipToPreviousButtonControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$skipToPreviousButtonControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef SkipToPreviousButtonControllerRef = AutoDisposeProviderRef<void>;
+String _$skipToNextButtonControllerHash() =>
+    r'47d8f9dcbce73cd76d002412d5b8e8d7dfe412b4';
+
+/// See also [skipToNextButtonController].
+@ProviderFor(skipToNextButtonController)
+final skipToNextButtonControllerProvider = AutoDisposeProvider<void>.internal(
+  skipToNextButtonController,
+  name: r'skipToNextButtonControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$skipToNextButtonControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef SkipToNextButtonControllerRef = AutoDisposeProviderRef<void>;
 String _$prevNextButtonControllerHash() =>
     r'7ef37a538cc10e9d4290c7d35a6974a7650bd057';
 
