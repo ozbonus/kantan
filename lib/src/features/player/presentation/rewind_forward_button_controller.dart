6432cfa815d1e94f2640f074d0cde4989d7f9fa2
Christopher Perry <ozbonus@gmail.com>
2025-04-15 12:53:09 +0800

    Simplify controllers.

--- lib/src/features/player/presentation/rewind_forward_button_controller.dart
+++ lib/src/features/player/presentation/rewind_forward_button_controller.dart
@@ -1,7 +1,18 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kantan/src/features/player/application/audio_handler_service.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'rewind_forward_button_controller.g.dart';
+
+@riverpod
+void rewindButtonController(Ref ref) {
+  ref.read(audioHandlerProvider).requireValue.rewind();
+}
+
+@riverpod
+void fastForwardButtonController(Ref ref) {
+  ref.read(audioHandlerProvider).requireValue.fastForward();
+}
 
 @riverpod
 class RewindForwardButtonController extends _$RewindForwardButtonController {
