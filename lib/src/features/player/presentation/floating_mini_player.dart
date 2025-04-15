d5c750c06bdfb31e8f6547039fc59ffe488a8573
Christopher Perry <ozbonus@gmail.com>
2025-04-15 12:52:31 +0800

    Temporarily disable button.

--- lib/src/features/player/presentation/floating_mini_player.dart
+++ lib/src/features/player/presentation/floating_mini_player.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:kantan/src/features/player/presentation/play_pause_button.dart';
 import 'package:kantan/src/routing/app_router.dart';
 
 class FloatingMiniPlayer extends ConsumerWidget {
@@ -14,7 +13,7 @@ class FloatingMiniPlayer extends ConsumerWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        const PlayPauseButton(),
+        // const PlayPauseButton(),
         IconButton.filledTonal(
           onPressed: () => context.goNamed(AppRoute.player),
           icon: const Icon(Icons.window_rounded),
