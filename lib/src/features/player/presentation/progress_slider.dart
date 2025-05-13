88649e5f103ca2353665a17bb3f6b8839abea778
Christopher Perry <ozbonus@gmail.com>
2025-05-13 15:23:03 +0800

    Wrap in styled container.

--- lib/src/features/player/presentation/progress_slider.dart
+++ lib/src/features/player/presentation/progress_slider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kantan/src/features/player/presentation/buttons.dart';
 import 'package:kantan/src/features/player/presentation/progress_slider_controller.dart';
 
 class ProgressSlider extends ConsumerWidget {
@@ -9,18 +10,23 @@ class ProgressSlider extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final positionData = ref.watch(progressSliderControllerProvider);
-    return ProgressBar(
-      progress: positionData.position,
-      total: positionData.duration,
-      timeLabelLocation: TimeLabelLocation.sides,
-      timeLabelTextStyle: Theme.of(context).textTheme.titleLarge?.copyWith(
-        fontFeatures: [
-          FontFeature.tabularFigures(),
-        ],
+    return ButtonContainer(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ProgressBar(
+          progress: positionData.position,
+          total: positionData.duration,
+          timeLabelLocation: TimeLabelLocation.sides,
+          timeLabelTextStyle: Theme.of(context).textTheme.titleLarge?.copyWith(
+            fontFeatures: [
+              FontFeature.tabularFigures(),
+            ],
+          ),
+          // Calling this onSeekProvider is a workaround for an issue described
+          // in detail in progress_slider_controller.dart.
+          onSeek: (position) => ref.read(onSeekProvider(position)),
+        ),
       ),
-      // Calling this onSeekProvider is a workaround for an issue described in
-      // detail in progress_slider_controller.dart.
-      onSeek: (position) => ref.read(onSeekProvider(position)),
     );
   }
 }
--- lib/src/features/player/presentation/speed_slider.dart
+++ lib/src/features/player/presentation/speed_slider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kantan/src/features/player/presentation/buttons.dart';
 import 'package:kantan/src/features/player/presentation/speed_slider_controller.dart';
 
 class SpeedSlider extends ConsumerWidget {
@@ -14,37 +15,42 @@ class SpeedSlider extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final speedValue = ref.watch(speedProvider);
-    return Row(
-      children: [
-        Text(
-          speedValue.when(
-            loading: () => _speedValueString(context, 1.0),
-            error: (_, __) => _speedValueString(context, 1.0),
-            data: (speed) => _speedValueString(context, speed),
-          ),
-          style: Theme.of(context).textTheme.titleLarge?.copyWith(
-            fontFeatures: [
-              FontFeature.tabularFigures(),
-            ],
-          ),
-        ),
-        Expanded(
-          child: Directionality(
-            textDirection: TextDirection.ltr,
-            child: Slider(
-              min: 0.5,
-              max: 2.0,
-              divisions: 16,
-              value: speedValue.when(
-                loading: () => 1.0,
-                error: (_, __) => 1.0,
-                data: (speed) => speed,
+    return ButtonContainer(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          children: [
+            Text(
+              speedValue.when(
+                loading: () => _speedValueString(context, 1.0),
+                error: (_, __) => _speedValueString(context, 1.0),
+                data: (speed) => _speedValueString(context, speed),
               ),
-              onChanged: (speed) => ref.read(setSpeedProvider(speed)),
+              style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                fontFeatures: [
+                  FontFeature.tabularFigures(),
+                ],
+              ),
             ),
-          ),
+            Expanded(
+              child: Directionality(
+                textDirection: TextDirection.ltr,
+                child: Slider(
+                  min: 0.5,
+                  max: 2.0,
+                  divisions: 16,
+                  value: speedValue.when(
+                    loading: () => 1.0,
+                    error: (_, __) => 1.0,
+                    data: (speed) => speed,
+                  ),
+                  onChanged: (speed) => ref.read(setSpeedProvider(speed)),
+                ),
+              ),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
