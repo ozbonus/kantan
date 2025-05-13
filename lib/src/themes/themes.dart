04762c67b216a5331eea568575a280209a069d24
Christopher Perry <ozbonus@gmail.com>
2025-05-13 12:39:59 +0800

    Use font from asset package.

--- lib/src/themes/themes.dart
+++ lib/src/themes/themes.dart
@@ -2,8 +2,12 @@ import 'package:flex_color_scheme/flex_color_scheme.dart';
 
 final lightTheme = FlexThemeData.light(
   scheme: FlexScheme.mandyRed,
+  applyElevationOverlayColor: false,
+  fontFamily: 'Atkinson Hyperlegible Next',
 );
 
 final darkTheme = FlexThemeData.dark(
   scheme: FlexScheme.mandyRed,
+  applyElevationOverlayColor: false,
+  fontFamily: 'Atkinson Hyperlegible Next',
 );
