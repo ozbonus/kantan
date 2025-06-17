27001b0ab3b88deff5ed08d12666195c8c55e6e5
Christopher Perry <ozbonus@gmail.com>
2025-06-17 17:13:39 +0800

    Remove unused import.

--- lib/src/kantan_player_app.dart
+++ lib/src/kantan_player_app.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_localized_locales/flutter_localized_locales.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
