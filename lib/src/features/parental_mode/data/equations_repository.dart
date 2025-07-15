a1bed374cfdd205923f1cf8ffbd992a8e68d28cc
Christopher Perry <ozbonus@gmail.com>
2025-07-15 15:33:31 +0800

    Add a list of equations.

---
+++ lib/src/features/parental_mode/data/equations_repository.dart
@@ -0,0 +1,11 @@
+import 'package:kantan/src/features/parental_mode/domain/equation.dart';
+
+const equationsList = <Equation>[
+  (tex: r'\frac{\sqrt{100}}{1} + 4^2 = x', ans: '26'),
+  (tex: r'x - \frac{\sqrt{49}}{1} = 14', ans: '21'),
+  (tex: r'\frac{1}{\sqrt{4}} \times 64 = x', ans: '32'),
+  (tex: r'\sqrt{x} + 1 = 7', ans: '36'),
+  (tex: r'\sqrt{81} \times 2 = x', ans: '18'),
+  (tex: r'5^2 + x = 36', ans: '11'),
+  (tex: r'2^5 = x', ans: '32'),
+];
