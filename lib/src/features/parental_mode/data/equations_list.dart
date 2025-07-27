854becf9efbf1a02ae474e642f13f353d84f670c
Christopher Perry <ozbonus@gmail.com>
2025-07-27 16:22:04 +0800

    Add a few more equations.

--- lib/src/features/parental_mode/data/equations_list.dart
+++ lib/src/features/parental_mode/data/equations_list.dart
@@ -7,4 +7,8 @@ const equationsList = <Equation>[
   (tex: r'\sqrt{x} + 1 = 7', ans: '36'),
   (tex: r'\sqrt{81} \times 2 = x', ans: '18'),
   (tex: r'5^2 + x = 36', ans: '11'),
+  (tex: r'\frac{45}{8} \times \sqrt{9} = x', ans: '15'),
+  (tex: r'77 + x = 10^2', ans: '23'),
+  (tex: r'\sqrt{x} \times 2 = 10', ans: '25'),
+  (tex: r'8^2 +4 = x', ans: '70'),
 ];
